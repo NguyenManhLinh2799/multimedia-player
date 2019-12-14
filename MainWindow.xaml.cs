3407599c7894bf1767d4ac356e2e5c4880d5131a
Tran Nhut Kha <trannhutkha123456@gmail.com>
2019-12-14 11:37:36 +0700

    set event click for button add, play, pause

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace multimedia_player
 {
@@ -20,9 +24,98 @@ namespace multimedia_player
     /// </summary>
     public partial class MainWindow : Window
     {
+        MediaPlayer Player = new MediaPlayer();
+        DispatcherTimer timer;
+        int currentIndex = -1;
         public MainWindow()
         {
             InitializeComponent();
+            Player.MediaEnded += Player_MediaEnded;
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (Player.Source != null)
+            {
+                var filename = FullPaths[currentIndex].Name;
+                //var converter = new NameConverter();
+                //var shortname = converter.Convert(filename, null, null, null);
+
+                var currentPos = Player.Position.ToString(@"mm\:ss");
+                var duration = Player.NaturalDuration.TimeSpan.ToString(@"mm\:ss");
+
+                currenttime.Content = $"{currentPos}/{duration}";
+                
+                int a = (Player.Position.Minutes * 60 + Player.Position.Seconds);
+                //Slider.Maximum = a;
+                //Slider.Value += 1;
+                //Slider.
+            }
+            else
+                Title = "No file selected...";
+        }
+
+        private void Player_MediaEnded(object sender, EventArgs e)
+        {
+            currentIndex++;
+            PlaySelectedIndex(currentIndex);
+        }
+
+        BindingList<FileInfo> FullPaths = new BindingList<FileInfo>();
+        private void addFile_Click(object sender, RoutedEventArgs e)
+        {
+            var screen = new OpenFileDialog();
+            if(screen.ShowDialog()==true)
+            {
+                var info = new FileInfo(screen.FileName);
+                FullPaths.Add(info);
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ListBoxPlaylist.ItemsSource = FullPaths;
+        }
+
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {
+            if(ListBoxPlaylist.SelectedIndex>=0)
+            {
+                currentIndex = ListBoxPlaylist.SelectedIndex;
+                PlaySelectedIndex(currentIndex);
+            }
+            else
+            {
+                MessageBox.Show("No file selected!");
+            }
+        }
+
+        bool isPlaying=false;
+        private void PlaySelectedIndex(int i)
+        {
+            string filename = FullPaths[i].FullName;
+
+            Player.Open(new Uri(filename, UriKind.Absolute));
+
+            Player.Play();
+            isPlaying = true;
+            timer.Start();
+        }
+
+        private void Pause_Click(object sender, RoutedEventArgs e)
+        {
+            if (isPlaying)
+            {
+                Player.Pause();
+            }
+            else
+            {
+                Player.Play();
+            }
+            isPlaying = !isPlaying;
         }
     }
 }
